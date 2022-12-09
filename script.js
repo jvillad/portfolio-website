c14965cff1849a2c2834d6b5f159e73aad565d7f
jvillad <jyvillad@gmail.com>
2022-12-09 16:21:09 +1100

    update

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
     
+function init() {
 
-hamburger.addEventListener('click', (ev) => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
+    hamburger.addEventListener('click', (ev) => {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+    
+    const navItem = document.querySelectorAll(".nav-item");
+        navItem.forEach(item => item.addEventListener('click', (ev) => {
+            hamburger.classList.remove('active');
+            navMenu.classList.remove('active');
+    
+        }))
+}
 
-document.querySelectorAll(".nav-item")
-    .forEach(item => item.addEventListener('click', (ev) => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-
-    }))
+init();
